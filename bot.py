ed250dfb6a8d675b4c18acfeb38842ed5ef38a92
kkristianr <kristian.ristic@gmail.com>
2024-08-27 12:59:53 +0200

    PoC ragu-bot

---
+++ bot.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import openai
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+st.set_page_config(page_title="Samu-Ragù", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+st.subheader("Chat with Samu-Ragù, l'agente virtuale che offre soluzioni ai tuoi dubbi 💬")
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Chiedi qualsiasi cosa sui nostro prodotti e servizi. Ask me any question about our products and services.",
+        }
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+    docs = reader.load_data()
+    Settings.llm = OpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0.2,
+        system_prompt="""You are an sales agent for the company TrabÀ, azienda italiana, è specializzata nella progettazione e nella realizzazione di sedie di design moderno in metallo e legno. Keep your answers technical and based on 
+        facts – respond in the language of the question - do not hallucinate features.""",
+    )
+    index = VectorStoreIndex.from_documents(docs)
+    return index
+
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question", verbose=True, streaming=True
+    )
+
+if prompt := st.chat_input(
+    "Ask a question"
+):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:  # Write message history to UI
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response_stream = st.session_state.chat_engine.stream_chat(prompt)
+        st.write_stream(response_stream.response_gen)
+        message = {"role": "assistant", "content": response_stream.response}
+        # Add response to message history
+        st.session_state.messages.append(message)
